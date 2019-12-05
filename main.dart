6f5ce7f297601f38b11f412b3b49184986f4c0e6
Laertes Junior <laertes.pj@gmail.com>
2019-12-04 23:35:31 -0300

    Comitando primeiro projeto

---
+++ main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:gerenciador_contas/ui/home_page.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
